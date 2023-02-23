bbb480e9cef5938b8bd3e5fe81820816bdf3d316
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-23 16:15:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import "./index.css";
+
+import Introduction from './components/Introduction';
+import Container from './ui/Container';
+import FlexBoard from './ui/FlexBoard';
+
 const App = () => {
   return (
-    <div>
-      Learn react
-    </div>
+    <Container>
+      <FlexBoard className="items-start justify-center">
+        <Introduction />
+      </FlexBoard>
+    </Container>
   )
 }
 
