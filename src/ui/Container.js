9944719930126a3c38f7082538be42fcbb8d28fb
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-24 16:41:40 +0300

    Fix issues with container

--- src/ui/Container.js
+++ src/ui/Container.js
@@ -5,11 +5,11 @@ const mainColor = `bg-black`;
 const Container = (props) => {
   const { children } = props;
   return (
-    <main className='container flex justify-center items-center'>
-      <div className={`${mainColor} w-4/5 h-4/5 border rounded-lg min-w-[10rem]`}>
+    <div className='flex justify-center w-screen h-[90%] items-center'>
+      <div className={`${mainColor} w-4/5 h-[98%] border rounded-lg min-w-[10rem]`}>
         {children}
       </div>
-    </main>
+    </div>
   );
 };
 
